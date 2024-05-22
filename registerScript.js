16e63518ee6f6918b5a82a7c50a3248bdff5e21e
M KARTIKEYA DATTA <kartikeyadatta3@gmail.com>
2024-05-22 13:34:32 -0500

    updated the login and register page with Firebase Register and login authentication
    Here we have linked our web application with the firebase and added an authentication feature to it.

--- registerScript.js
+++ registerScript.js
@@ -1,20 +1,73 @@
 // register.js
 
-document.getElementById('registrationForm').addEventListener('submit', function(event) {
+// document.getElementById('registrationForm').addEventListener('submit', function(event) {
+//     event.preventDefault();
+//     // Get user input from the registration form
+//     var username = document.getElementById('username').value;
+//     var password = document.getElementById('password').value;
+//     // Add more fields as needed
+
+//     // Perform registration logic here
+//     // You can use AJAX to send registration data to your server-side code
+//     // Example:
+//     // var formData = new FormData();
+//     // formData.append('username', username);
+//     // formData.append('password', password);
+//     // Perform AJAX request to your server to handle registration
+
+//     // After successful registration, you can redirect the user to the login page or any other page
+//     window.location.href = 'index.html'; // Redirect to the login page
+// });
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-app.js";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+} from "https://www.gstatic.com/firebasejs/10.12.1/firebase-auth.js";
+//import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+//import {} from "https://www.gstatic.com/firebasejs/8.10.1/firebase-firestore.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBHNHnLgsm8HJ9-L4XUmIQ03bumJa3JZEE",
+  authDomain: "qrcodescanner-150cc.firebaseapp.com",
+  databaseURL: "https://qrcodescanner-150cc-default-rtdb.firebaseio.com",
+  projectId: "qrcodescanner-150cc",
+  storageBucket: "qrcodescanner-150cc.appspot.com",
+  messagingSenderId: "425306294564",
+  appId: "1:425306294564:web:c514a419f71dde9fc3cbb1",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+document
+  .getElementById("registrationForm")
+  .addEventListener("submit", function (event) {
     event.preventDefault();
-    // Get user input from the registration form
-    var username = document.getElementById('username').value;
-    var password = document.getElementById('password').value;
-    // Add more fields as needed
 
-    // Perform registration logic here
-    // You can use AJAX to send registration data to your server-side code
-    // Example:
-    // var formData = new FormData();
-    // formData.append('username', username);
-    // formData.append('password', password);
-    // Perform AJAX request to your server to handle registration
-
-    // After successful registration, you can redirect the user to the login page or any other page
-    window.location.href = 'index.html'; // Redirect to the login page
-});
+    const email = document.getElementById("Email").value;
+    const Firstname = document.getElementById("Firstname").value;
+    const Lastname = document.getElementById("Lastname").value;
+    const password = document.getElementById("password").value;
+    //console.log(email)
+    const auth = getAuth();
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed up
+        const user = userCredential.user;
+        alert("Creating Account...");
+        // After successful registration, you can redirect the user to the login page or any other page
+        window.location.href = "index.html"; // Redirect to the login page
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        alert(errorMessage);
+        // ..
+      });
+  });
--- script.js
+++ script.js
@@ -1,27 +1,105 @@
-    document.getElementById('loginForm').addEventListener('submit', function(event) {
-        event.preventDefault();
-        var username = document.getElementById('username').value;
-        var password = document.getElementById('password').value; 
-        if (username === 'instructor' && password === '12345') {
-            window.location.href = 'instructor_dashboard.html';
-        } else if (username === 'student' && password === '12345') { 
-            window.location.href = 'student_dashboard.html';
-        } else {
-            alert('Invalid username or password.');
+// document.getElementById('loginForm').addEventListener('submit', function(event) {
+//     event.preventDefault();
+//     var username = document.getElementById('username').value;
+//     var password = document.getElementById('password').value;
+//     if (username === 'instructor' && password === '12345') {
+//         window.location.href = 'instructor_dashboard.html';
+//     } else if (username === 'student' && password === '12345') {
+//         window.location.href = 'student_dashboard.html';
+//     } else {
+//         alert('Invalid username or password.');
+//     }
+// });
+
+// document.getElementById('instructorLogin').addEventListener('click', function(event) {
+//     event.preventDefault();
+//     document.getElementById('username').value = 'instructor';
+//     document.getElementById('password').value = '12345';
+// });
+
+// function logout() {
+//     // Implement logout logic here
+//     window.location.href = 'index.html';
+// }
+
+// document.getElementById('loginForm').addEventListener('submit', function(event) {
+//     event.preventDefault();
+//     var username = document.getElementById('username').value;
+//     var password = document.getElementById('password').value;
+//     if (username === 'instructor' && password === '123456') {
+//         window.location.href = 'instructor_dashboard.html';
+//     } else if (username === 'student' && password === '123456') {
+//         window.location.href = 'student_dashboard.html';
+//     } else {
+//         alert('Invalid username or password.');
+//     }
+// });
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-app.js";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+} from "https://www.gstatic.com/firebasejs/10.12.1/firebase-auth.js";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBHNHnLgsm8HJ9-L4XUmIQ03bumJa3JZEE",
+  authDomain: "qrcodescanner-150cc.firebaseapp.com",
+  databaseURL: "https://qrcodescanner-150cc-default-rtdb.firebaseio.com",
+  projectId: "qrcodescanner-150cc",
+  storageBucket: "qrcodescanner-150cc.appspot.com",
+  messagingSenderId: "425306294564",
+  appId: "1:425306294564:web:c514a419f71dde9fc3cbb1",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+document
+  .getElementById("loginForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const email = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    //console.log(email)
+    const auth = getAuth();
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        var username = document.getElementById("username").value;
+        var password = document.getElementById("password").value;
+        if (username === "instructor" && password === "123456") {
+          window.location.href = "instructor_dashboard.html";
         }
-    });
+        if (
+          username === "fellahazizh@nwmissouri.edu" &&
+          password === "1234567"
+        ) {
+          window.location.href = "instructor_dashboard.html";
+        } else if (username === "student" && password === "123456") {
+          window.location.href = "student_dashboard.html";
+        } else {
+          alert("Invalid username or password.");
+        }
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        alert(errorMessage);
+      });
+  });
+document
+  .getElementById("instructorLogin")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    document.getElementById("username").value = "instructor";
+    document.getElementById("password").value = "12345";
+  });
 
-    document.getElementById('instructorLogin').addEventListener('click', function(event) {
-        event.preventDefault();
-        document.getElementById('username').value = 'instructor';
-        document.getElementById('password').value = '12345';
-    });
-
-   
-    
-    function logout() {
-        // Implement logout logic here
-        window.location.href = 'index.html';
-    }
-
-
+function logout() {
+  // Implement logout logic here
+  window.location.href = "index.html";
+}
